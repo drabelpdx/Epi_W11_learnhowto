1289c4ac959f8d6624a9dccd042df74f4103fb16
david abel <student@epicodus.com>
2015-06-30 10:16:51 -0700

    Adds edit to lesson.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -23,8 +23,22 @@ class LessonsController < ApplicationController
     end
   end
 
+  def edit
+    @lesson = Lesson.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @lesson= Lesson.find(params[:id])
+    if @lesson.update(lesson_params)
+      redirect_to lessons_path
+    else
+      render :edit
+    end
+  end
+
   private
   def lesson_params
-    params.require(:lesson).permit(:name, :description)
+    params.require(:lesson).permit(:name, :content)
   end
 end
